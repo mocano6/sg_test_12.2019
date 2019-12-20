abcc94ec3efcbc8ea13b63f02cd671376adf47d7
moicano6 <lukaszferszt@gmail.com>
2019-12-20 19:55:27 +0100

    first task - options add

--- src/input.js
+++ src/input.js
@@ -1,8 +1,18 @@
 // Elements
-const inputVal = document.querySelector('#city');
-const citys = ['Poland', 'Germany', 'Spain', 'France'];
+const inputVal = document.querySelector('#country');
+const countries = ['Poland', 'Germany', 'Spain', 'France'];
+const dList = document.querySelector('#countries');
 
 export default () => {
+
+  function dataList(data) {
+    data.forEach(element => {
+      let option = document.createElement('option');
+      option.value = element;
+      dList.appendChild(option);
+    });
+  }
+  dataList(countries);
 
   function data(arr) {
     const compareEl = inputVal.value.toUpperCase();
@@ -15,8 +25,8 @@ export default () => {
 
   inputVal.addEventListener('keyup', (e)=>{
     if (e.keyCode === 13) {
-      //data(citys);
-      console.log(data(citys));
+      //data(countries);
+      console.log(data(countries));
     }
   });
 };
